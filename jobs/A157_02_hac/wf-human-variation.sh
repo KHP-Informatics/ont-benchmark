3dc8455f831f8bc114893af014f1d0a7e49fa358
Renato Santos <renato.santos@kcl.ac.uk>
2024-05-13 15:01:40 +0100

    Create wf-basecalling and wf-human-variation jobs Job files for 1st batch of samples sequenced

---
+++ jobs/A157_02_hac/wf-human-variation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=A157_02_hac_wf-human-variation
+#SBATCH --partition=nd_bioinformatics_cpu,cpu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8G
+#SBATCH --time=2-00:00:00
+#SBATCH --output=/scratch/users/%u/slurm_jobs/%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=renato.santos@kcl.ac.uk
+#SBATCH --chdir /scratch/prj/ppn_als_longread/jobs/A157_02_hac
+
+module load nextflow/22.10.1-gcc-13.2.0
+
+# Set the Singularity and Nextflow cache directories
+export NXF_HOME=/scratch/users/${USER}/nextflow/
+export NXF_CACHE=/scratch/users/${USER}/nextflow/cache
+export NXF_TEMP=/scratch/users/${USER}/nextflow/tmp
+export NXF_SINGULARITY_CACHEDIR=/scratch/users/${USER}/singularity/
+export SINGULARITY_CACHEDIR=/scratch/users/${USER}/singularity/
+
+# Specify Nextflow max heap size
+export NFX_OPTS="-Xms512M -Xmx8G"
+
+nextflow run epi2me-labs/wf-human-variation \
+    -r v2.1.0 \
+    -c /scratch/prj/ppn_als_longread/config/wf-human-variation.config \
+    --bam /scratch/prj/ppn_als_longread/basecalled/A157_02_hac \
+    --out_dir /scratch/prj/ppn_als_longread/vcf/A157_02_hac \
+    --sample_name A157_02_hac \
+    --sex male \
+    --basecaller_cfg dna_r10.4.1_e8.2_400bps_hac@v4.3.0
