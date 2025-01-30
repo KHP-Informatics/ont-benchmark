f4e41d8ca801c3fe60a1408780d88132ac4952e8
Renato Santos <renato.santos@kcl.ac.uk>
2025-01-30 10:24:41 +0000

    Update wf-basecalling and wf-human variation jobs
    Updated nextflow version and log file extension

---
+++ jobs/ont/A153_01_sup/wf-basecalling.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=A153_01_sup_wf-basecalling
+#SBATCH --partition=nd_bioinformatics_cpu,cpu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=4G
+#SBATCH --time=2-00:00:00
+#SBATCH --output=/scratch/users/%u/slurm_jobs/%j_%x.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=renato.santos@kcl.ac.uk
+#SBATCH --chdir /scratch/prj/ppn_als_longread/jobs/A153_01_sup
+
+module load nextflow/24.10.2-gcc-13.2.0
+
+export NXF_HOME=/scratch/users/${USER}/nextflow/
+export NXF_CACHE=/scratch/users/${USER}/nextflow/cache
+export NXF_TEMP=/scratch/users/${USER}/nextflow/tmp
+export NXF_SINGULARITY_CACHEDIR=/scratch/users/${USER}/singularity/
+export SINGULARITY_CACHEDIR=/scratch/users/${USER}/singularity/
+export NXF_JVM_ARGS="-XX:InitialRAMPercentage=25 -XX:MaxRAMPercentage=75"
+
+nextflow run epi2me-labs/wf-basecalling \
+    -r v1.1.7 \
+    -c /scratch/prj/ppn_als_longread/config/wf-basecalling.config \
+    --input /scratch/prj/ppn_als_longread/pod5/A153_01 \
+    --out_dir /scratch/prj/ppn_als_longread/basecalled/A153_01_sup \
+    --sample_name A153_01_sup \
+    --basecaller_cfg dna_r10.4.1_e8.2_400bps_sup@v4.3.0 \
+    --remora_cfg dna_r10.4.1_e8.2_400bps_sup@v4.3.0_5mCG_5hmCG@v1
---
+++ jobs/ont/A154_06_sup/wf-human-variation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=A154_06_sup_wf-human-variation
+#SBATCH --partition=nd_bioinformatics_cpu,cpu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=4G
+#SBATCH --time=2-00:00:00
+#SBATCH --output=/scratch/users/%u/slurm_jobs/%j_%x.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=renato.santos@kcl.ac.uk
+#SBATCH --chdir /scratch/prj/ppn_als_longread/jobs/A154_06_sup
+
+module load nextflow/24.10.2-gcc-13.2.0
+
+export NXF_HOME=/scratch/users/${USER}/nextflow/
+export NXF_CACHE=/scratch/users/${USER}/nextflow/cache
+export NXF_TEMP=/scratch/users/${USER}/nextflow/tmp
+export NXF_SINGULARITY_CACHEDIR=/scratch/users/${USER}/singularity/
+export SINGULARITY_CACHEDIR=/scratch/users/${USER}/singularity/
+export NXF_JVM_ARGS="-XX:InitialRAMPercentage=25 -XX:MaxRAMPercentage=75"
+
+nextflow run epi2me-labs/wf-human-variation \
+    -r v2.1.0 \
+    -c /scratch/prj/ppn_als_longread/config/wf-human-variation.config \
+    --bam /scratch/prj/ppn_als_longread/basecalled/A154_06_sup \
+    --out_dir /scratch/prj/ppn_als_longread/vcf/A154_06_sup \
+    --sample_name A154_06_sup \
+    --sex male \
+    --basecaller_cfg dna_r10.4.1_e8.2_400bps_sup@v4.3.0
