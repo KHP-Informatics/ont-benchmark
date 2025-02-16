b8cba2e41d0f9f8bbf370c1ea95546e32a3c74b7
Renato Santos <renato.santos@kcl.ac.uk>
2025-02-16 12:31:41 +0000

    Updated wf-basecalling and wf-human-variation jobs.
    Deleted hac jobs.
    Updated nextflow and pipeline versions.

--- jobs/ont/A154_06_sup/wf-basecalling.sh
+++ jobs/ont/A154_06_sup/wf-basecalling.sh
@@ -5,11 +5,11 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=4G
-#SBATCH --time=2-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --output=/scratch/users/%u/slurm_jobs/%j_%x.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=renato.santos@kcl.ac.uk
-#SBATCH --chdir /scratch/prj/ppn_als_longread/jobs/A154_06_sup
+#SBATCH --chdir /scratch_tmp/prj/ppn_als_longread/wf-basecalling/jobs/ont-benchmark/A154_06_sup
 
 module load nextflow/24.10.2-gcc-13.2.0
 
@@ -21,10 +21,10 @@ export SINGULARITY_CACHEDIR=/scratch/users/${USER}/singularity/
 export NXF_JVM_ARGS="-XX:InitialRAMPercentage=25 -XX:MaxRAMPercentage=75"
 
 nextflow run epi2me-labs/wf-basecalling \
-    -r v1.1.7 \
-    -c /scratch/prj/ppn_als_longread/config/wf-basecalling.config \
-    --input /scratch/prj/ppn_als_longread/pod5/A154_06 \
-    --out_dir /scratch/prj/ppn_als_longread/basecalled/A154_06_sup \
+    -r v1.4.5 \
+    -c /scratch_tmp/prj/ppn_als_longread/wf-basecalling/config/wf-basecalling.config \
+    --input /scratch_tmp/prj/ppn_als_longread/wf-basecalling/pod5/ont-benchmark/A154_06 \
+    --out_dir /scratch_tmp/prj/ppn_als_longread/wf-basecalling/basecalled/A154_06_sup \
     --sample_name A154_06_sup \
-    --basecaller_cfg dna_r10.4.1_e8.2_400bps_sup@v4.3.0 \
-    --remora_cfg dna_r10.4.1_e8.2_400bps_sup@v4.3.0_5mCG_5hmCG@v1
+    --basecaller_cfg dna_r10.4.1_e8.2_400bps_sup@v5.0.0 \
+    --remora_cfg dna_r10.4.1_e8.2_400bps_sup@v5.0.0_5mCG_5hmCG@v3
--- jobs/ont/A160_96_sup/wf-human-variation.sh
+++ jobs/ont/A160_96_sup/wf-human-variation.sh
@@ -5,11 +5,11 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=4G
-#SBATCH --time=2-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --output=/scratch/users/%u/slurm_jobs/%j_%x.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=renato.santos@kcl.ac.uk
-#SBATCH --chdir /scratch/prj/ppn_als_longread/jobs/A160_96_sup
+#SBATCH --chdir /scratch_tmp/prj/ppn_als_longread/wf-human-variation/jobs/ont-benchmark/A160_96_sup
 
 module load nextflow/24.10.2-gcc-13.2.0
 
@@ -20,11 +20,16 @@ export NXF_SINGULARITY_CACHEDIR=/scratch/users/${USER}/singularity/
 export SINGULARITY_CACHEDIR=/scratch/users/${USER}/singularity/
 export NXF_JVM_ARGS="-XX:InitialRAMPercentage=25 -XX:MaxRAMPercentage=75"
 
+# See https://github.com/nextflow-io/nextflow/issues/2695#issuecomment-1635939435
+nohup lsof +D /scratch_tmp/prj/ppn_als_longread/work -r 600 &> /dev/null &
+LSOF_PID=$!
+trap "kill $LSOF_PID" EXIT
+
 nextflow run epi2me-labs/wf-human-variation \
-    -r v2.1.0 \
-    -c /scratch/prj/ppn_als_longread/config/wf-human-variation.config \
-    --bam /scratch/prj/ppn_als_longread/basecalled/A160_96_sup \
-    --out_dir /scratch/prj/ppn_als_longread/vcf/A160_96_sup \
+    -r v2.6.0 \
+    -c /scratch_tmp/prj/ppn_als_longread/wf-human-variation/config/wf-human-variation.config \
+    --bam /scratch_tmp/prj/ppn_als_longread/wf-basecalling/basecalled/A160_96_sup \
+    --out_dir /scratch_tmp/prj/ppn_als_longread/wf-human-variation/vcf/A160_96_sup \
     --sample_name A160_96_sup \
-    --sex female \
-    --basecaller_cfg dna_r10.4.1_e8.2_400bps_sup@v4.3.0
+    --sex XX \
+    --override_basecaller_cfg dna_r10.4.1_e8.2_400bps_sup@v5.0.0
