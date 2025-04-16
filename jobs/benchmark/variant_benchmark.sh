43259da5bccbb9c22b9262f8095d32267ff96b67
Renato Santos <renato.santos@kcl.ac.uk>
2025-04-16 14:37:32 +0100

    Update variant_benchmark job
    Fixed work directory path and nextflow run repo

--- jobs/benchmark/variant_benchmark.sh
+++ jobs/benchmark/variant_benchmark.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=4G
-#SBATCH --time=0-04:00:00
+#SBATCH --time=0-06:00:00
 #SBATCH --output=/scratch/users/%u/slurm_jobs/%j_%x.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=renato.santos@kcl.ac.uk
@@ -17,7 +17,7 @@ export NXF_TEMP=/scratch/users/${USER}/nextflow/tmp
 export NXF_SINGULARITY_CACHEDIR=/scratch/users/${USER}/singularity/
 export SINGULARITY_CACHEDIR=/scratch/users/${USER}/singularity/
 export NXF_JVM_ARGS="-XX:InitialRAMPercentage=25 -XX:MaxRAMPercentage=75"
-export NXF_WORK=/scratch_tmp/prj/ppn_als_longread/work
+export NXF_WORK=/scratch/prj/ppn_als_longread/ont-benchmark/work
 
 module load nextflow/24.10.2-gcc-13.2.0
 
@@ -26,4 +26,4 @@ nohup lsof +D ${NXF_WORK} -r 600 &> /dev/null &
 LSOF_PID=$!
 trap "kill $LSOF_PID" EXIT
 
-nextflow run main.nf
+nextflow run renatosantos98/ont-benchmark
